c82b117193c2a13a099c956818fe7205fb274670
LucasEBSkora <skora.lucas@gmail.com>
2020-04-13 18:28:36 -0300

    possivelmente versão final - que orgulho do meu bebê

--- balls.py
+++ balls.py
@@ -3,6 +3,7 @@ from pygame.locals import *
 
 from OpenGL.GL import *
 from OpenGL.GLU import *
+from OpenGL.GLUT import *
 
 # import pygame
 # from pygame.locals import *
@@ -11,6 +12,10 @@ from OpenGL.GLU import *
 # from OpenGL.GLU import gluPerspective
 
 from math import ceil, sin, cos, pi
+
+add = 0.02
+K = 2
+N = 1
 
 def draw_axis():
   glBegin(GL_LINES)
@@ -66,23 +71,53 @@ def draw_ball(K, N): #K -> raio da bola; N-> norma N-ésima usada
     theta += pi/4
   glEnd()
 
+def render_ui():
 
 
-def main():
+  glPushMatrix()
+  glLoadIdentity()
+
+  render_text("||V||    = (|x|   + |y|   + |z|    )    = K", (-0.95, 0.9), GLUT_BITMAP_TIMES_ROMAN_24)
+
+  n = "{:.2f}".format(N)
+
+  render_text(n, (-0.88, 0.88), GLUT_BITMAP_TIMES_ROMAN_10)
+  render_text(n, (-0.765, 0.94), GLUT_BITMAP_TIMES_ROMAN_10)
+  render_text(n, (-0.67, 0.94), GLUT_BITMAP_TIMES_ROMAN_10)
+  render_text(n, (-0.575, 0.94), GLUT_BITMAP_TIMES_ROMAN_10)
+  
+  render_text("1", (-0.511, 0.955), GLUT_BITMAP_TIMES_ROMAN_10)
+  render_text("_", (-0.525, 0.95), GLUT_BITMAP_TIMES_ROMAN_10)
+  render_text("_", (-0.518, 0.95), GLUT_BITMAP_TIMES_ROMAN_10)
+  render_text("_", (-0.511, 0.95), GLUT_BITMAP_TIMES_ROMAN_10)
+  render_text("_", (-0.504, 0.95), GLUT_BITMAP_TIMES_ROMAN_10)
+  render_text("_", (-0.496, 0.95), GLUT_BITMAP_TIMES_ROMAN_10)
+  render_text(n, (-0.520, 0.915), GLUT_BITMAP_TIMES_ROMAN_10)
+
+  glPopMatrix()
+
+def render_text(text, text_pos, font, color=(1., 1., 1.)):
+  glColor(*color)
+  glRasterPos2f(*text_pos)
+  for char in text:
+    glutBitmapCharacter(font, ord(char))
+
+
+def main(): 
+  global N 
+  global add
   pygame.init()
-  display = (800, 600)
+  glutInit(sys.argv)
+  display = (1280, 720)
   pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
-  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-  glEnable(GL_BLEND);
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+  glEnable(GL_BLEND)
   gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
   glTranslatef(.0, .0, -10)
   glRotatef(-80, 1, 0, 0)
   glRotatef(-135, 0, 0, 1)
   
-  add = 0.02
-  K = 2
-  N = 1
-
+  input("aperte enter para começar")
   while True:
     if (N <= 1) :
       N = 1
@@ -99,6 +134,7 @@ def main():
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
     draw_axis()
     draw_ball(K, N)
+    render_ui()
     pygame.display.flip()
     pygame.time.wait(10)
     N += add*(N**2)
